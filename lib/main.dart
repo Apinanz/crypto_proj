9679a8e3090f4d8339932854a65009daff00bf41
Natthavut Apinantakun <apinantakun_n@silpakorn.edu>
2021-11-09 23:01:53 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'APIX Crypto',
       theme: ThemeData(
         textTheme: const TextTheme(
           headline1: TextStyle(
