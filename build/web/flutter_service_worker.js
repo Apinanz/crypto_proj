67b3b0cd51b430ca8e666a8597b640fef53565d9
Natthavut Apinantakun <apinantakun_n@silpakorn.edu>
2021-11-10 22:28:12 +0700

    initial commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -67,8 +67,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "427341da3c9e0b827e490739b3d27e39",
-"/": "427341da3c9e0b827e490739b3d27e39",
+"index.html": "85080b6a895fee34f5acf5399208f8bd",
+"/": "85080b6a895fee34f5acf5399208f8bd",
 "main.dart.js": "76870ee33a0cb234026a89f191b21a2e",
 "manifest.json": "4affdb58e4233587559e0e976ed48125",
 "version.json": "eb1a9a10f733882a0c323cc099f61c26"
